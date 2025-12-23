5993f62f51d66bbe5cea4a2ef585014fbe7a071c
SabirKazi135 <sabirkazi135@gmail.com>
2025-12-23 20:49:50 +0530

    commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const { generateRandomnumber, celciusToFah } = require("./utils");
+import { getPost } from "./postController.js";
 
-console.log(`Randome Number: ${generateRandomnumber()}`);
+// const { generateRandomnumber, celciusToFah } = require("./utils");
 
-console.log(`Cel to f  ${celciusToFah(0)}`);
+// console.log(`Randome Number: ${generateRandomnumber()}`);
+
+// console.log(`Cel to f  ${celciusToFah(0)}`);
+
+console.log(getPost());
--- postController.js
+++
@@ -1,7 +0,0 @@
-const posts = [
-  { id: 1, title: " post One" },
-  { id: 2, title: " post Two" },
-  { id: 3, title: " post Three" },
-];
-
-export const getPost = () => posts;
